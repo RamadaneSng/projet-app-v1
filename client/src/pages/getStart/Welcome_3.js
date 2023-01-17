a30fd403d82122d584bd8db71f7f4eb0e781447b
i2xv <iscoramadane@gmail.com>
2023-01-17 12:27:08 +0100

    v2

--- client/src/pages/getStart/Welcome_3.js
+++ client/src/pages/getStart/Welcome_3.js
@@ -5,7 +5,7 @@ const Welcome_3 = () => {
   return (
     <Welcome_centent
       content={
-        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, soluta"
+        "PRAMSK, pour ne plus galerer chercher une place de parking"
       }
       link={"/inscription"}
     />
